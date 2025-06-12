72fca3c6ca9d7fa79162f82ecf8ee1a6e61efae5
jonx <xtechjawad@gmail.com>
2025-06-12 09:25:38 +0500

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -8,7 +8,7 @@ const { setCommitHash, getCommitHash } = require('../data/updateDB');
 cmd({
     pattern: "update",
     alias: ["upgrade", "sync"],
-    react: '🆕',
+    react: '🚀',
     desc: "Update the bot to the latest version.",
     category: "misc",
     filename: __filename
@@ -19,7 +19,7 @@ cmd({
         await reply("🔍 Checking for KHAN-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/JawadTechXD/IK/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/JawadTechXD/KHAN-MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
@@ -33,7 +33,7 @@ cmd({
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/JawadTechXD/IK/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/JawadTechXD/KHAN-MD/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "IK-main");
+        const sourcePath = path.join(extractPath, "KHAN-MD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
