f1df8bc2c6eedf78bebdc472fd9adb07c763176d
jonx <xtechjawad@gmail.com>
2025-09-21 09:34:46 +0500

    Update tool-tts.js

--- plugins/tool-tts.js
+++ plugins/tool-tts.js
@@ -7,7 +7,7 @@ cmd({
     pattern: "tts",
     desc: "download songs",
     category: "download",
-    react: "👧",
+    react: "💀",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
@@ -18,7 +18,7 @@ if(!q) return reply("Need some text.")
   slow: false,
   host: 'https://translate.google.com',
 })
-await conn.sendMessage(from, { audio: { url: url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek })
+await conn.sendMessage(from, { audio: { url: url }, mimetype: 'audio/mpeg', ptt: false }, { quoted: mek })
     }catch(a){
 reply(`${a}`)
 }
