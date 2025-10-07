b78e4441e881041662208520f02cb9fba66d32b7
jawi <jawadytxd@gmail.com>
2025-10-07 14:55:40 +0500

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -4,8 +4,8 @@ const { sleep } = require('../lib/functions');
 
 cmd({
     pattern: "update",
-    alias: ["sync", "up"],
-    react: "📡",
+    alias: ["sync", "up", "r", "reboot", "restart"],
+    react: "🚀",
     desc: "update the bot",
     category: "owner",
     filename: __filename
@@ -22,10 +22,25 @@ async (conn, mek, m, {
             return reply("🚫 *This command is only for the bot owner (creator).*");
         }
 
-        const { exec } = require("child_process");
-        reply("♻️ Updating the bot...");
+        // Send react immediately
+        await conn.sendMessage(from, { react: { text: '⏳', key: m.key } });
+        
+        // Wait 500ms
+        await sleep(500);
+        
+        // Send update message
+        await reply("*♻️ Updating and restarting the bot*...");
+        
+        // Send ✅ react after message
+        await conn.sendMessage(from, { react: { text: '✅', key: m.key } });
+        
+        // Wait 1500ms after sending message and react
         await sleep(1500);
+        
+        // Execute restart
+        const { exec } = require("child_process");
         exec("pm2 restart all");
+        
     } catch (e) {
         console.log(e);
         reply(`${e}`);
