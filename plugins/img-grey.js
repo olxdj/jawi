551208f5cf4ce6ed2dfb64bb4c94ba01aeafbcf9
jawi <jawadytxd@gmail.com>
2025-10-13 09:35:08 +0500

    Update img-grey.js

--- plugins/img-grey.js
+++ plugins/img-grey.js
@@ -19,7 +19,7 @@ cmd({
   alias: ["greyedit"],
   react: '📸',
   desc: "Scan and remove bg from images",
-  category: "img_edit",
+  category: "media",
   use: ".grey [reply to image]",
   filename: __filename
 }, async (conn, message, m,  { reply, mek }) => {
