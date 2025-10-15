575995b9a898d98df284ad1772076bc2606e6155
jawi <jawadytxd@gmail.com>
2025-10-16 04:41:55 +0500

    Create tool-shutdown.js

---
+++ plugins/tool-shutdown.js
@@ -0,0 +1,16 @@
+const { cmd ,commands } = require('../command');
+const { exec } = require('child_process');
+const config = require('../config');
+const {sleep} = require('../lib/functions')
+// 1. Shutdown Bot
+cmd({
+    pattern: "shutdown",
+    desc: "Shutdown the bot.",
+    category: "owner",
+    react: "🛑",
+    filename: __filename
+},
+async (conn, mek, m, { from, isOwner, reply }) => {
+    if (!isOwner) return reply("❌ You are not the owner!");
+    reply("🛑 Shutting down...").then(() => process.exit());
+});
